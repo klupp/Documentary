5837590290c710803f128072f25fcba8668b0548
vikipeeva <peeva.viki@gmail.com>
2020-06-13 13:44:03 +0200

    alter previous commit

--- create_project_script.py
+++ create_project_script.py
@@ -1,13 +1,14 @@
-import os
+import sys, os
 
 
 def main(argv):
     path = argv[0] # get the path to the project from the arguments
     try:
         os.makedirs(path) # create directory where the dat project will be kept
+        os.mkdir(path+"/.dat")
     except FileExistsError:
-        print("Directory " , dirName ,  " already exists. Choose another name")
-    
+        print("Directory " , path ,  " already exists. Choose another name.")
+    os.chdir(path)
 
 
 if __name__ == "__main__":
