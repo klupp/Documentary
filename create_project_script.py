feca3f0a4fa09f3fec2a41592bc206c5ea664268
vikipeeva <peeva.viki@gmail.com>
2020-06-13 14:38:46 +0200

    tables added

--- create_project_script.py
+++ create_project_script.py
@@ -1,14 +1,22 @@
 import sys, os
+import pandas as pd
 
 
 def main(argv):
     path = argv[0] # get the path to the project from the arguments
     try:
         os.makedirs(path) # create directory where the dat project will be kept
-        os.mkdir(path+"/.dat")
+        os.mkdir(path + "/.dat") # inside create the .dat folder
+        os.chdir(path + "/.dat") # change directory
     except FileExistsError:
         print("Directory " , path ,  " already exists. Choose another name.")
-    os.chdir(path)
+    pages = pd.DataFrame(columns=["pageId", "format", "pageNum"])
+    pages.to_csv("pages.csv", index=False)
+    layers = pd.DataFrame(columns=["layerId", "format", "ref"])
+    layers.to_csv("layers.csv", index=False)
+    pageLayerMap = pd.DataFrame(columns = ["pageId", "layerId", "order"])
+    pageLayerMap.to_csv("pageLayerMap.csv", index=False)
+
 
 
 if __name__ == "__main__":
