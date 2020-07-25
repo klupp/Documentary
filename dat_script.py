f631fea26e13542e18c2294f7d06404119d22b87
Aleksandar Kuzmanoski <aleksandarkuzmanoski11@gmail.com>
2020-07-25 22:12:41 +0200

    - dat remove page called create function fix.

--- dat_script.py
+++ dat_script.py
@@ -17,13 +17,13 @@ refresh = subparsers.add_parser('refresh', parents=[reload_data_script.parser], 
 add = subparsers.add_parser("add")
 add_subparsers = add.add_subparsers(title="Add commands",
                                     description="List of all available `dat add` commands.", dest="add_command")
-add_subparsers.add_parser("page", aliases=['p'], parents=[create_page_script.parser], add_help=False)
-add_subparsers.add_parser("data", aliases=['d'], parents=[load_data_script.parser], add_help=False)
+add_subparsers.add_parser("page", parents=[create_page_script.parser], add_help=False)
+add_subparsers.add_parser("data", parents=[load_data_script.parser], add_help=False)
 
 remove = subparsers.add_parser("remove")
 remove_subparsers = remove.add_subparsers(title='Remove commands',
                                           description="List of all available `dat remove` commands.", dest="remove_command")
-remove_subparsers.add_parser("page", aliases=['p'], parents=[remove_page_script.parser], add_help=False)
+remove_subparsers.add_parser("page", parents=[remove_page_script.parser], add_help=False)
 
 
 def add_command(args):
@@ -36,7 +36,7 @@ def add_command(args):
 
 def remove_command(args):
     switcher = {
-        "page": create_page_script.main
+        "page": remove_page_script.main
     }
     switcher.get(args.remove_command)(args)
 
